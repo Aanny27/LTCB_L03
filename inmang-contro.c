97842ec7dba39ed0171fdbb16742d613226cb9a2
PhQuan <125746911+Kanh27@users.noreply.github.com>
2025-03-15 00:35:25 +0700

    Update inmang-contro.c

--- inmang-contro.c
+++ inmang-contro.c
@@ -3,10 +3,12 @@
 
 int main()
 {
-    int *a[10] = {1,2,3,4,5,6,7,8,9,10};
+    int a[10] = {1,2,3,4,5,6,7,8,9,10}, *ptr;
+    ptr = a;
     for(int i = 0; i < 10; i++)
     {
-        printf("%d ", *(a + i));
+        printf("%d ",  *(ptr + i));
     }
     return 0;
 }
+
