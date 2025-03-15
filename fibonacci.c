02ae2544782b23edc02f8dc1d2f654071a5ff7e9
PhQuan <quanmundangyeu2006@gmail.com>
2025-03-15 14:40:43 +0700

    PQ

--- fibonacci.c
+++ fibonacci.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #define ll long long
 
 ll fib(int n)
 {
-    in
-    if(n == 0) 
-        return 0;
-    if(n == 1)
-        return 0;
+    if(n == 0) return 0;
+    if(n == 1) return 0;
+    int a = 1, b = 0, c;
     while(n--)
     {
-
+        c = a;
+        a += b;
+        b = c;
     }
+    return b;
 }
 int main()
 {
-    int n, value;
+    int n;
     printf("Nhap vao n: ");
     scanf("%d", &n);
-    printf("gia tri cua so thu n trong day fibonacci la: %d", value);
+    fib(n);
+    printf("gia tri cua so thu n trong day fibonacci la: %d", fib(n));
     return 0;
+    //Aanny
 }
