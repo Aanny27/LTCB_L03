dd6d57191a6d852054cf564d2f6e2f192bf2a281
PhQuan <125746911+Aanny27@users.noreply.github.com>
2025-03-25 01:25:56 +0700

    Create bai3vonglap.c

---
+++ bai3vonglap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define ll long long
+
+int main()
+{
+    for(int i = 1; i <= 20; i++)
+    {
+        if(i % 2 == 0)
+            printf("%d  ", i);
+    }
+    return 0;
+}
